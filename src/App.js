f3fb9d671f689d789b28423291cab3f22de68f04
Muhammad Yasin <m.yaseensalim@gmail.com>
2024-05-04 07:44:16 -0700

    Corrected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import {Route,Routes} from 'react-router-dom'
+import {Route,Routes} from 'react-router-dom';
+import {Box} from "@mui/material";
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Exercise from './pages/ExerciseDetail';
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Box width="400px" sx={{width :{ xl:'1488px' }}} m="auto" >
+      <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/exercise/:id' element={<Exercise/>}/>
+     </Routes>
+     <Footer/>
+    </Box>
   );
 }
 
