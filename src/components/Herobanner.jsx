431d86e6cf597b7d5709565bf2c90d3a6dc751e8
Muhammad Yasin <m.yaseensalim@gmail.com>
2024-05-06 11:26:10 -0700

    file

---
+++ src/components/Herobanner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Herobanner=()=>{
+    return(
+        <div>
+            Herobanner
+        </div>
+    )
+}
+
+export default Herobanner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-
+import {Box} from "@mui/material"
+import Herobanner from "../components/Herobanner";
+import SearchExercise from "../components/SearchExercise";
+import Exercise from "./ExerciseDetail";
 const Home=()=>{
     return(
-        <div>
-            Home
-        </div>
+        <Box>
+            <Herobanner/>
+            <SearchExercise/>
+            <Exercise/>
+        </Box>
     )
 }
 
